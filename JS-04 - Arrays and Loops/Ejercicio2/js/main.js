45ba79f0160ae9abb5ffa30cb00ed57ca99ce0f2
Melina <meli.mun.med@gmail.com>
2023-03-27 23:36:35 -0600

    Segundo ejercicio de la serie 4

---
+++ JS-04 - Arrays and Loops/Ejercicio2/js/main.js
@@ -0,0 +1,7 @@
+for (let asterisco = 1; asterisco <=5; ++ asterisco ){
+    let resultado = '';
+    for(let variable = 1; variable <= asterisco; ++ variable){
+    resultado += '* '; 
+    }
+    console.log(resultado);
+}
