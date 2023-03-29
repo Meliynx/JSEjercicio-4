7ff98395af903ac325d05b0d4eed686722955791
Melina <meli.mun.med@gmail.com>
2023-03-28 19:33:08 -0600

    tercera parte del tercer ejercicio de la serie 4, dos posibles soluciones, ya estan comentadas en el codigo

--- JS-04 - Arrays and Loops/Ejercicio3/js/main.js
+++ JS-04 - Arrays and Loops/Ejercicio3/js/main.js
@@ -1,8 +1,15 @@
-let xValue = 100; //Dado que la funcion decrementa, no se puede empezar en 0
+/*let xValue = 100; //Dado que la funcion decrementa, no se puede empezar en 0
 while (xValue>0){
-    if (xValue % 2 !==0) /* Segunda parte del tercer ejercicio, aqui se compara si el residuo es diferente de 0 y si es el caso, se imprime el valor de xValue en la consola*/
-    {console.log(xValue)};
+    if (xValue % 2 !==0)  //Segunda parte del tercer ejercicio, aqui se compara si el residuo es diferente de 0 y si es el caso, se imprime el valor de xValue en la consola
+    {console.log("[", xValue, "]")}; //Tercera parte complementando las instrucciones anteriores
     xValue -= 0.5;
-}
+}*/
 
- 
+ let xValue = 1;
+ let n = prompt ("Cuantos valores necesitas?",0);
+
+while (xValue<=n){
+    console.log("[", xValue, "]"); //Tercera parte haciendolo por separado, es necesario anotar el valor de "n" que se desee
+    xValue ++;
+    
+} 
