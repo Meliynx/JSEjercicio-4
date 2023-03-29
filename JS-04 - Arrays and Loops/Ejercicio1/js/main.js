abf54e4bd84e686ee300c68f98b9a6be8b8311b4
Melina <meli.mun.med@gmail.com>
2023-03-28 19:11:07 -0600

    notas faltantes en ejercicio 1 y adelanto de segunda parte del ekercicio 3 de la serie 4

--- JS-04 - Arrays and Loops/Ejercicio1/js/main.js
+++ JS-04 - Arrays and Loops/Ejercicio1/js/main.js
@@ -1,12 +1,12 @@
-let fila = ["Sofia", "David", "Juan"];
+let fila = ["Sofia", "David", "Juan"]; // los primeros integrantes de la fila
 console.log("Parte #1");
 console.log(fila);
-fila.push("Sara");
-fila.push("Agustin");
-fila.shift();
+fila.push("Sara"); //Agregamos a Sara al final de la fila
+fila.push("Agustin"); // Agregamos a Agustin al final de la fila
+fila.shift(); //Sofia es removidad e la fila ya quepasa a ventanilla
 console.log("Parte #2");
 console.log(fila);
-fila.splice(1,0, "Renata");
-fila.push("Elena");
+fila.splice(1,0, "Renata"); //Agregamos a Renata al espacio especifico de la fila 
+fila.push("Elena"); // Agregamos a Elena al fina de la fila
 console.log("Parte #3");
 console.log(fila);
--- JS-04 - Arrays and Loops/Ejercicio3/js/main.js
+++ JS-04 - Arrays and Loops/Ejercicio3/js/main.js
@@ -1,5 +1,8 @@
 let xValue = 100; //Dado que la funcion decrementa, no se puede empezar en 0
 while (xValue>0){
-    console.log(xValue)
+    if (xValue % 2 !==0) /* Segunda parte del tercer ejercicio, aqui se compara si el residuo es diferente de 0 y si es el caso, se imprime el valor de xValue en la consola*/
+    {console.log(xValue)};
     xValue -= 0.5;
 }
+
+ 
